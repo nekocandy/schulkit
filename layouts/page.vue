408ae6e6be62db9d4a44630cc698549fb3828e31
Piyush <bhangalepiyush@gmail.com>
2023-05-27 18:23:58 +0530

    feat(layout): add default layout

---
+++ layouts/page.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="bg-white">
+    <div class="mx-auto h-screen max-w-7xl flex flex-col gap-6 py-6">
+      <div class="flex-1">
+        <slot />
+      </div>
+
+      <div>
+        <BottomBar />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
