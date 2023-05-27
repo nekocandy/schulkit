507bc6e4c1167faa35218023009d63ece94ac80c
Piyush <bhangalepiyush@gmail.com>
2023-05-27 15:24:43 +0530

    fix: try fixinf cf deployment

--- composables/useRealmApp.ts
+++ composables/useRealmApp.ts
@@ -3,7 +3,7 @@ import * as Realm from 'realm-web'
 export function useRealmApp() {
   const config = useRuntimeConfig()
   const realmConfig = {
-    id: import.meta.env.SSR ? config.realmAppId : config.public.realmAppId,
+    id: (import.meta.env && import.meta.env.SSR) ? config.realmAppId : config.public.realmAppId,
   }
   return new Realm.App(realmConfig)
 }
