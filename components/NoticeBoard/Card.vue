b25a39d8311a857f874c2db62f35746025bfa977
Piyush <bhangalepiyush@gmail.com>
2023-05-28 00:48:05 +0530

    feat: notice board

---
+++ components/NoticeBoard/Card.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+defineProps({
+  title: String,
+  description: String,
+  date: String,
+  totalStudents: Number,
+  enrolledStudents: Number,
+})
+</script>
+
+<template>
+  <div class="h-fit flex flex-col justify-between gap-8 rounded-xl bg-[#ffcc00] px-4 py-4">
+    <div class="flex flex-col gap-2">
+      <div class="font-bold">
+        {{ $props.title }}
+      </div>
+
+      <div class="pt-4">
+        {{ $props.description }}
+      </div>
+
+      <div class="flex items-center gap-1 text-[#dc2828]">
+        <div class="font-bold">
+          Date:
+        </div>
+
+        <div>
+          {{ $props.date }}
+        </div>
+      </div>
+    </div>
+
+    <div class="flex items-center justify-between rounded-xl bg-[#ffe168] px-4 py-2">
+      <div class="flex items-center gap-2">
+        {{ enrolledStudents }} / {{ totalStudents }} students
+
+        <div class="i-tabler-arrow-right" />
+      </div>
+
+      <div>
+        <button class="rounded-xl bg-white px-4 py-2">
+          Volunteer
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/notice-board.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'page',
+})
+</script>
+
+<template>
+  <div class="h-full w-full flex flex-col gap-4 pb-2">
+    <div class="pb-0 pt-4 text-3xl font-bold">
+      Notice Board
+    </div>
+
+    <div class="grid grid-cols-3 h-full w-full flex-1 gap-4 rounded-xl bg-[#f22c2c] px-4 py-4">
+      <NoticeBoardCard
+        title="FOOD DONATION DRIVE NOTICE"
+        description="Please join us in our Food Donation Drive to help those in need within our community. We are accepting non-perishable food items at the designated drop-off location on 5th June for 3 hours. Your contributions can make a significant impact on the lives of individuals and families facing food insecurity."
+        date="2021-10-10"
+        :total-students="100"
+        :enrolled-students="50"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
