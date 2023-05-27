124d4d9e91236af8ccabc4786bb9bf9a2574c84d
Piyush <bhangalepiyush@gmail.com>
2023-05-27 21:03:32 +0530

    feat: external links

---
+++ components/Dashboard/ExternalLinks.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { faker } from '@faker-js/faker'
+
+const foodItemsList = [
+  'Moodle',
+  'Syllabus',
+]
+
+const items = new Array(2).fill(0).map(_ => ({
+  id: faker.number.int({
+    min: 10,
+    max: 99,
+  }),
+  item: faker.helpers.arrayElement(foodItemsList),
+}))
+
+const foodItems = ref(items)
+</script>
+
+<template>
+  <div class="w-full flex flex-col rounded-xl bg-[#a5a5a5] text-white">
+    <div class="flex items-center justify-between rounded-t-xl bg-[#6b6b6b] px-6 py-3">
+      <div class="i-tabler-external-link h-6 w-6" />
+      <div>External Links</div>
+    </div>
+
+    <div class="grid grid-cols-1 gap-2 px-6 py-4 text-black">
+      <div v-for="item in foodItems" :key="item.id">
+        <div class="w-full flex items-center gap-4">
+          <div class="rounded-md bg-[#cbf8a7] px-4 py-2 font-bold text-[#457b1b]">
+            {{ item.id }}
+          </div>
+
+          <div class="flex">
+            <li>
+              <div list-disc />
+            </li>
+
+            {{ item.item }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -7,8 +7,14 @@ definePageMeta({
 <template>
   <div class="h-full w-full flex gap-8">
     <div class="w-full flex flex-col gap-4">
-      <DashboardTodoList class="h-full max-w-2/6" />
-      <DashboardCanteen class="max-w-1/2" />
+      <div class="h-full w-full flex gap-8">
+        <DashboardTodoList class="h-full max-w-2/6" />
+        <DashboardTodoList class="h-full max-w-4/6" />
+      </div>
+      <div class="h-fit flex gap-8">
+        <DashboardCanteen class="w-full" />
+        <DashboardExternalLinks class="max-w-2/6" />
+      </div>
     </div>
 
     <div class="w-1/2 flex flex-col gap-4">
