d41c1200831d7ec73cc6fca66d416fcab99941f1
Piyush <bhangalepiyush@gmail.com>
2023-05-27 23:12:56 +0530

    feat: working pastebin service

--- pages/paste.vue
+++ pages/paste.vue
@@ -1,12 +1,32 @@
 <script setup lang="ts">
+import { BSON } from 'realm-web'
+
 definePageMeta({
   layout: 'page',
 })
-
+const route = useRoute()
+const realmApp = useRealmApp()
+const highlighter = await useHighlighter()
 const code = ref('')
+const isViewMode = ref(false)
+const htmlCode = ref('')
+
+const documentCode = computed(() => route.query.code)
 
 async function saveCode() {
+  const codeCollection = useCollection('codes')
 
+  const insertedDocument = await codeCollection.insertOne({
+    code: code.value,
+    userId: realmApp.currentUser!.id,
+    createdAt: new Date(),
+  })
+
+  navigateTo({
+    query: {
+      code: insertedDocument.insertedId.toString(),
+    },
+  })
 }
 
 async function newCode() {
@@ -16,6 +36,22 @@ async function newCode() {
 async function editCode() {
 
 }
+
+onBeforeMount(() => {
+  if (!realmApp.currentUser)
+    console.error('User not logged in')
+})
+
+onMounted(async () => {
+  if (documentCode) {
+    const codeCollection = useCollection('codes')
+    const document = await codeCollection.findOne({ _id: new BSON.ObjectId(documentCode.value?.toString()) })
+    code.value = document.code
+    isViewMode.value = true
+
+    htmlCode.value = highlighter.codeToHtml(code.value, {})
+  }
+})
 </script>
 
 <template>
@@ -25,7 +61,7 @@ async function editCode() {
     </div>
 
     <div class="relative h-full rounded-xl bg-[#ffcc00] px-8 py-8">
-      <div class="absolute right-8 top-8 flex items-center gap-2 rounded-bl-xl rounded-tr-xl bg-black px-4 py-2 text-white">
+      <div class="absolute right-8 top-8 z-20 flex items-center gap-2 border rounded-bl-xl rounded-tr-xl bg-black px-4 py-2 text-white">
         <button @click="saveCode()">
           <div class="i-tabler-device-floppy h-6 w-6" />
         </button>
@@ -36,11 +72,18 @@ async function editCode() {
           <div class="i-tabler-file-pencil h-6 w-6" />
         </button>
       </div>
-      <textarea v-model="code" class="h-full w-full resize-none rounded-xl px-4 py-4 placeholder-text-[#646464]" placeholder="// enter code here" />
+      <div class="relative h-full w-full">
+        <div v-if="isViewMode" class="absolute bottom-0 left-0 right-0 top-0 h-full w-full">
+          <div class="h-full rounded-xl" v-html="htmlCode" />
+        </div>
+        <textarea v-else v-model="code" class="h-full w-full resize-none rounded-xl px-4 py-4 placeholder-text-[#646464]" placeholder="// enter code here" />
+      </div>
     </div>
   </div>
 </template>
 
-<style scoped>
-
+<style>
+.shiki {
+  @apply h-full w-full flex-1 px-4 py-4 rounded-xl;
+}
 </style>
