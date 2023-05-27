a3f7d1d8c4315c10220bac9d5b51da568e3b5b1f
Piyush <bhangalepiyush@gmail.com>
2023-05-27 21:53:04 +0530

    feat: add username

--- pages/index.vue
+++ pages/index.vue
@@ -5,21 +5,25 @@ definePageMeta({
 </script>
 
 <template>
-  <div class="h-full w-full flex gap-4">
-    <div class="w-full flex flex-col gap-4">
-      <div class="h-full w-full flex gap-4">
-        <DashboardTodoList class="h-full max-w-2/6" />
-        <DashboardCalendar class="h-full max-w-4/6" />
-      </div>
-      <div class="h-fit flex gap-4">
-        <DashboardCanteen class="w-full" />
-        <DashboardExternalLinks class="max-w-2/6" />
-      </div>
+  <div class="h-full w-full flex flex-col">
+    <div class="py-4 text-3xl">
+      Welcome, <span class="font-bold">sugoii</span>
     </div>
-
-    <div class="w-1/2 flex flex-col gap-4">
-      <DashboardWeatherCard />
-      <DashboardUpcomingTA />
+    <div class="h-full w-full flex gap-4 overflow-y-auto">
+      <div class="w-full flex flex-col gap-4">
+        <div class="h-full w-full flex gap-4">
+          <DashboardTodoList class="h-full max-w-2/6" />
+          <DashboardCalendar class="h-full max-w-4/6" />
+        </div>
+        <div class="h-fit flex gap-4">
+          <DashboardCanteen class="w-full" />
+          <DashboardExternalLinks class="max-w-2/6" />
+        </div>
+      </div>
+      <div class="w-1/2 flex flex-col gap-4">
+        <DashboardWeatherCard />
+        <DashboardUpcomingTA />
+      </div>
     </div>
   </div>
 </template>
