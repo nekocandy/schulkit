432961f5dba085a79c43a95d08dd55db73cc5456
Piyush <bhangalepiyush@gmail.com>
2023-05-28 09:01:23 +0530

    feat: tnt test page

---
+++ components/TNT/TestInfoCard.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+  description: string
+  type: 'essay'
+}>()
+</script>
+
+<template>
+  <div class="h-fit flex flex-col justify-between gap-4 rounded-xl bg-[#ffe168] px-4 py-4">
+    <div class="flex flex-col gap-2">
+      <div class="font-bold">
+        {{ $props.title }}
+      </div>
+
+      <div>
+        {{ $props.description }}
+      </div>
+    </div>
+
+    <div class="flex items-center justify-between rounded-xl bg-[#ffe168] py-2">
+      <div class="flex items-center gap-1 text-[#dc2828]">
+        <div class="font-bold">
+          Type:
+        </div>
+
+        <div>
+          {{ $props.type }}
+        </div>
+      </div>
+
+      <div>
+        <button class="rounded-xl bg-white px-4 py-2">
+          Start
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/tnt/tests.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'page',
+})
+</script>
+
+<template>
+  <div class="h-full w-full flex flex-col gap-4 pb-2">
+    <div class="pb-0 pt-4 text-3xl font-bold">
+      Tests
+    </div>
+
+    <div class="grid grid-cols-3 h-full w-full flex-1 gap-4 rounded-xl bg-[#ffcc00] px-4 py-4">
+      <TNTTestInfoCard
+        title="The American Revolution was a major turning point in world history. "
+        description="What were the causes of the American Revolution?"
+        type="essay"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
