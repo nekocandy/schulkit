ca942ed6bef4f72671bbd5b9b0b4467af4d797f7
Piyush <bhangalepiyush@gmail.com>
2023-05-28 09:15:58 +0530

    feat: essay test page

---
+++ layouts/yellow.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="bg-[#ffcc00] font-arvo">
+    <div class="mx-auto h-screen max-w-7xl flex flex-col gap-2 py-2">
+      <div class="flex-1">
+        <slot />
+      </div>
+
+      <div>
+        <BottomBar />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/tnt/test/essay.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'yellow',
+})
+const endTime = dayjs().add(1, 'hour')
+const timeLeft = ref('')
+
+useIntervalFn(() => {
+  const { $toast } = useNuxtApp()
+
+  $toast.info('Saving data...')
+}, 5000)
+
+onMounted(() => {
+  setInterval(() => {
+    const minutesLeft = endTime.diff(dayjs(), 'minute')
+    const secondsLeft = endTime.diff(dayjs(), 'second') - minutesLeft * 60
+    timeLeft.value = `${minutesLeft}:${secondsLeft.toString().padStart(2, '0')}`
+  }, 1000)
+})
+</script>
+
+<template>
+  <div class="h-full w-full flex flex-col">
+    <div flex items-end gap-2 pl-6>
+      <div class="rounded-t-xl bg-[#f22c2c] px-8 py-1 text-lg font-bold text-white">
+        Essay
+      </div>
+      <div class="rounded-t-xl bg-[#2c37d8] px-8 py-1 text-lg font-bold text-white">
+        {{ timeLeft }}
+      </div>
+    </div>
+
+    <div class="border-2 border-black rounded-xl bg-white px-4 py-4">
+      The American Revolution was a major turning point in world history. What were the causes of the American Revolution?
+    </div>
+
+    <div class="h-full w-full flex-1 pt-4">
+      <textarea class="h-full w-full resize-none rounded-lg px-4 py-4" placeholder="enter your answer here" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
